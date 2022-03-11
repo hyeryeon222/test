ba46268ac31558933b63996a7e2e82d38a182bef
anhyeryeon2 <100520490+anhyeryeon2@users.noreply.github.com>
2022-03-11 17:20:54 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Dessert{
+private:
+    int onega;
+    int panmaga;
+    string name;
+    int v;
+    bool panmaing;
+public:
+    string getname(){return name;}
+    int getonega(){return onega;}
+    int getpanmaga(){return panmaga;}
+    int getv(){return v;}
+    bool getpanmaing(){return panmaing;}
+
+    void setname(int n){name=n;}
+    void setonega(int one){onega=one;}
+    void setpanmaga(int p){panmaga=p;}
+    void setv(int ev){
+        if(ev<0||ev>100)
+            v=100;
+        else
+            v=ev;}
+            void setpanmaing(bool ing){panmaing=ing;
+    }
+
+};
+
+int main(){
+    Dessert myd;
+    myd.setname("mintchoco");
+    myd.setpanmaga(3000);
+    myd.setonega(1200);
+    myd.setv(200);
+    myd.setpanmaing("?꾩옱?먮ℓ以?);
+
+    cout<<myd.getname()<<' ';
+    cout<<myd.getpanmaga()<<' ';
+    cout<<myd.getonega()<<' ';
+    cout<<myd.getv()<<' ';
+    cout<<myd.getpanmaing()<<' ';
+    return 0;
+}
+
